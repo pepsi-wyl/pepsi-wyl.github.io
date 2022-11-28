28dc64a8780fe447193d09afa7b0f34b6f80b655
pepsi-wyl <2322535585@qq.com>
2022-11-28 12:13:00 +0800

    Site updated: 2022-11-28 12:12:59

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "10243",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "c19f0d03a68f4a0cb213af44b2c8daf3"
+  }
+}
